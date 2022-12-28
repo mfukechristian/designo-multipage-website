6cba995057b5b578855a5c5ac369a1550c1a2630
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2022-12-28 03:35:17 +0200

    final commit

---
+++ src/footer/Footer.js
@@ -0,0 +1,11 @@
+// import Aside from "../sharedComponents/AsideFooter";
+import MainFooter from "../sharedComponents/MainFooter";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <MainFooter />
+    </div>
+  );
+};
+
+export default Footer;
